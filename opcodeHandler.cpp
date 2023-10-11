b1ab4cce4217e086492859704dc15b76941127c3
Kyle Jin Jones <32524565+KyleJinJones@users.noreply.github.com>
2023-10-11 14:27:31 -0700

    added basic tests for opcode handler

--- opcodeHandler.cpp
+++ opcodeHandler.cpp
@@ -1,20 +1,23 @@
 #include <string>
 #include <unordered_map>
+#include <iostream>
 #include <sstream>
+#include "hexConverter.h"
 
 //TODO ADD FULL OPCODETABLE
 //
 //TODO ADD FULL OPCODETABLE
 
 //opcode table is our hard coded hashmap of opcodes and their corresponding integer values
-std::unordered_map<std::string, int> opcodeTable{
-    {"MUL",20},
-    {"ADD",18}
-    {"LDA",00}
+std::unordered_map<std::string, std::string> opcodeTable{
+    {"MUL","20"},
+    {"ADD","18"},
+    {"LDA","00"},
+    {"LDS","6C"}
   };
   
 //checks if a string is a valid opcode
-bool checkOpcode(string opcode){
+bool checkOpcode(std::string opcode){
     //if opcode in opcode table return true
     auto opcodeNumber = opcodeTable.find(opcode);
     if(opcodeNumber!= opcodeTable.end()){
@@ -25,21 +28,26 @@ bool checkOpcode(string opcode){
 }
 
 //returns the given opcode as a hex string
-string encodeOpcode(string opcode, bool nflag, bool iflag){
-    int opcodeNumber = opcodeTable[opcode];
+std::string encodeOpcode(std::string opcode, bool nflag, bool iflag){
+    std::string opcodeNumber = opcodeTable[opcode];
 
     //add to our opcode integer here to set n and i flags
     //if n should be set we add +2
     if(nflag){
-        opcodeNumber+=2;
+        opcodeNumber=toHex(toDec(opcodeNumber)+2,2);
     }
     //if i should be set we add +1
     if(iflag){
-        opcodeNumber+=1;
+        opcodeNumber=toHex(toDec(opcodeNumber)+1,2);
     }
 
-    std::stringstream strstream;
-    strstream<<std::setfill('0') << std::setw(2) <<std::hex << opcodeNumber; 
-    //return the converted hex string
-    return ( strstream.str() );
+    return opcodeNumber;
+}
+//Expected output: 6D 03 20 0 1
+int main(){
+    std::cout<<encodeOpcode("LDS",false,true)<<std::endl;
+    std::cout<<encodeOpcode("LDA",true,true)<<std::endl;
+    std::cout<<encodeOpcode("MUL",false,false)<<std::endl;
+    std::cout<<checkOpcode("ALU")<<std::endl;
+    std::cout<<checkOpcode("MUL")<<std::endl;
 }
