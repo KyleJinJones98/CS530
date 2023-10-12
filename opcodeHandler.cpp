d6b3bdfab46f5fb3e504c49b3132b9b379a2bae9
JVue4073 <114715871+JVue4073@users.noreply.github.com>
2023-10-12 13:55:41 -0700

    Update opcodeHandler.cpp
    Added the other missing Opcodes to the Opcode unordered map.

--- opcodeHandler.cpp
+++ opcodeHandler.cpp
@@ -5,16 +5,67 @@
 
 //Handles functionality related to the opcode table including checking for valid opcodes, and encoding opcodes
 
-//TODO ADD FULL OPCODETABLE
-//
-//TODO ADD FULL OPCODETABLE
-
 //opcode table is our hard coded hashmap of opcodes and their corresponding integer values
 std::unordered_map<std::string, std::string> opcodeTable{
     {"MUL","20"},
     {"ADD","18"},
     {"LDA","00"},
-    {"LDS","6C"}
+    {"LDS","6C"},
+    {"ADDF", "58"},
+    {"ADDR", "90"},
+    {"AND", "40"},
+    {"CLEAR", "B4"},
+    {"COMP", "28"},
+    {"COMPF", "88"},
+    {"COMPR", "A0"},
+    {"DIV", "24"},
+    {"DIVF", "64"},
+    {"DIVR", "9C"},
+    {"FIX", "C4"},
+    {"FLOAT", "C0"},
+    {"HIO", "F4"},
+    {"J", "3C"},
+    {"JEQ", "30"},
+    {"JGT", "34"},
+    {"JLT", "38"},
+    {"JSUB", "48"},
+    {"LDB", "68"},
+    {"LDCH", "50"},
+    {"LDF", "70"},
+    {"LDL", "08"},
+    {"LDT", "74"},
+    {"LDX", "04"},
+    {"LPS", "D0"},
+    {"MULF", "60"},
+    {"MULR", "98"},
+    {"NORM", "C8"},
+    {"OR", "44"},
+    {"RD", "D8"},
+    {"RMO", "AC"},
+    {"RSUB", "4C"},
+    {"SHIFTL", "A4"},
+    {"SHIFTR", "A8"},
+    {"SIO", "F0"},
+    {"SSK", "EC"},
+    {"STA", "0C"},
+    {"STB", "78"},
+    {"STCH", "54"},
+    {"STF", "80"},
+    {"STI", "D4"},
+    {"STL", "14"},
+    {"STS", "7C"},
+    {"STSW", "E8"},
+    {"STT", "84"},
+    {"STX", "10"},
+    {"SUB", "1C"},
+    {"SUBF", "5C"},
+    {"SUBR", "94"},
+    {"SVC", "B0"},
+    {"TD", "E0"},
+    {"TIO", "F8"},
+    {"TIX", "2C"},
+    {"TIXR", "B8"},
+    {"WD", "DC"}
   };
   
 //checks if a string is a valid opcode
