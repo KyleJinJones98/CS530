58c218d2e3b4556d0abfc33660c08d8f167321bb
Kyle Jin Jones <32524565+KyleJinJones@users.noreply.github.com>
2023-10-19 22:45:40 -0700

    added inline tag to utility functions

--- hexConverter.h
+++ hexConverter.h
@@ -7,13 +7,14 @@
 /*
 hex Converter is a Utility class used for converting values to and from hex
 */
-
+inline
 std::string toHex(int decnumber, int width){
   std::stringstream strstream;
   strstream<<std::setfill('0') << std::setw(width) <<std::hex <<std::uppercase<< decnumber; 
   return ( strstream.str() );
 }
 
+inline
 int toDec(std::string hexnumber){
     return stoi(hexnumber, 0, 16);
 }
