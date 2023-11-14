7b7a077b7ece00ca2ffdb2032bc974adc443b2ed
Kyle Jin Jones <32524565+KyleJinJones@users.noreply.github.com>
2023-11-14 10:25:53 -0800

    Altered Writing to File so that it looks better as target addresses and operations are longer

--- sourceLineStruct.h
+++ sourceLineStruct.h
@@ -82,10 +82,10 @@ struct sourceLineStruct
     //writes a source line to a given file
     void writeLine(std::ofstream &listingFile){
         if(label!="."){
-            listingFile << std::setw(5) << std::left << lineAddress<< std::setw(8) << std::left << label<< std::setw(7) << std::left << operation<< std::setw(8) << std::left << targetAddress<< std::setw(8) << std::left << hexInstruction<<std::endl;
+            listingFile << std::setw(4) << std::left << lineAddress<<" "<< std::setw(6) << std::left << label<<"  "<< std::setw(8) << std::left << operation<<"  "<< std::setw(10) << std::left << targetAddress<<" "<< std::setw(8) << std::left << hexInstruction<<std::endl;
         }
         else{
-            listingFile << std::setw(5) << std::left << label<< std::left <<operation<<std::endl;
+            listingFile << std::setw(4) << std::left << label <<" "<< std::left <<operation<<std::endl;
         }
     }
 
